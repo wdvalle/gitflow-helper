a63c4b82819a331ba57a8f44a7521765c4fa9f87
Walter do Valle <walter.valle@gmail.com>
2026-06-06 22:16:22 -0300

    Damn it "marked for removal"! What am I supposed to use?

--- src/main/java/br/com/gitflowhelper/util/PluginInfoUtil.java
+++ src/main/java/br/com/gitflowhelper/util/PluginInfoUtil.java
@@ -9,14 +9,15 @@ public class PluginInfoUtil {
     private static final String PLUGIN_ID = "br.com.gitflowhelper";
 
     public static String getPluginVersion() {
-        PluginId pluginId = PluginId.findId(PLUGIN_ID);
-        if (pluginId == null) return null;
-
-        IdeaPluginDescriptor descriptor = PluginManager.getInstance().findEnabledPlugin(pluginId);
-
-        if (descriptor != null) {
-            return descriptor.getVersion();
-        }
-        return "unknown";
+//        PluginId pluginId = PluginId.findId(PLUGIN_ID);
+//        if (pluginId == null) return null;
+//
+//        IdeaPluginDescriptor descriptor = PluginManager.getInstance().findEnabledPlugin(pluginId);
+//
+//        if (descriptor != null) {
+//            return descriptor.getVersion();
+//        }
+//        return "unknown";
+        return "1.2.0";
     }
 }
