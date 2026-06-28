a1e9ceafde51f33223a315b9321a9abac622b0a0
Walter do Valle <walter.valle@gmail.com>
2026-06-28 20:18:43 -0300

    Old connector removed

--- src/main/java/br/com/gitflow/tracker/old/IssueTrackerConnector.java
+++
@@ -1,8 +0,0 @@
-package br.com.gitflow.tracker.old;
-
-import com.intellij.tasks.Task;
-
-public interface IssueTrackerConnector {
-    void markAsStarted(Task task) throws Exception;
-    void markAsFinished(Task task) throws Exception;
-}
